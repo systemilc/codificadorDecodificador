2354f4d740a18243796917dd4108a21531108814
systemilc <email>
2021-12-08 22:54:45 -0300

    adicionado base64 no js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var showPassos = document.querySelector("#passos")
+var showPassos = document.querySelector(".chaves")
 var entradaDeTexto = document.querySelector('#entradaDeTexto')
 var saidadaDeTexto = document.querySelector('#saida')
 var radio = document.querySelectorAll('.codificar')
@@ -7,16 +7,24 @@ var botao = document.querySelector('#botao')
 
 botao.addEventListener('click', function(event){
     event.preventDefault()
-    if(radio[0].checked){
+    var tipoCodigo = document.querySelector("#tipoCodigo").value
+    if(radio[0].checked && tipoCodigo == 'asci'){
         var valTexto = entradaDeTexto.value.split('')
         var valPassos = parseInt(passos.value)
         saidadaDeTexto.value = encriptar(valTexto, valPassos) 
        
-    }else{
+    }else if(radio[1].checked && tipoCodigo == 'asci'){
         var valTexto = entradaDeTexto.value.split('')
         var valPassos = parseInt(passos.value)
         saidadaDeTexto.value = descriptar(valTexto, valPassos)
+    }else if(radio[0].checked && tipoCodigo == "base64"){
+        var valTexto = entradaDeTexto.value
+        saidadaDeTexto.value = btoa(valTexto)
+    }else if(radio[1].checked && tipoCodigo == "base64"){
+        var valTexto = entradaDeTexto.value
+        saidadaDeTexto.value = atob(valTexto)
     }
+    
 
 })
     
@@ -27,7 +35,7 @@ selecionar = document.addEventListener('click',  function(){
 var selecao = document.querySelector('#tipoCodigo').value
 if(selecao === "asci"){
     showPassos.style.display = "flex"
-    showPassos.style.flexDirection = 'row'
+    showPassos.style.flexDirection = 'column'
 
 }else{
     showPassos.style.display = 'none'
@@ -56,9 +64,9 @@ function descriptar(entrada, passos){
     return entrada.map((str)=>{
         var codificador = str.charCodeAt()
         if(codificador >=65 && codificador <=90){
-                return String.fromCharCode(((codificador-65 -passos)%26)+65)
+                return String.fromCharCode(((codificador-90 -passos)%26)+90)
         }else if(codificador >=97 && codificador <=122){
-            return String.fromCharCode(((codificador-97 -passos)%26)+97)
+            return String.fromCharCode(((codificador-122 -passos)%26)+122)
         }else{
             return str
         }
